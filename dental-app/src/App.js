357b12d2fe54eecb23a5dfddeba93269ebe40a3a
ChatoUrt <jonataniurrutiac@gmail.com>
2023-08-29 22:26:26 -0600

    Started a small frontEnd change in the app.css and app.js

--- dental-app/src/App.js
+++ dental-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>DENTAL EMANUEL</h1>
+        <p>La clínica dental Emanuel está trabajando en un proyecto online que sirva para hacer un registro online</p>
+
       </header>
+      <div className='card-container'>
+          <h1 className='title-card'>Nombre del paciente</h1>
+      </div>
     </div>
   );
 }
